07781ad514325bb82e3df58156c493581ab748a5
Wasiq Sheikh <wasiqsheikh1998@gmail.com>
2023-03-14 12:35:02 +0500

    Updated Server.js File

--- Connection/dbConnection.js
+++ Connection/dbConnection.js
@@ -4,7 +4,10 @@ mongoose.set('strictQuery', false);
 
 const ConnectDatabase = async() => {
     try {
-        const connect = await mongoose.connect(process.env.MONGODB_URL)
+        const connect = await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(() => console.log('Database Connected'));
     } catch (error) {
         console.log(error);
